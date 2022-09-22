ee86cd06a678a83bc140a20e9ab4cd9d1ff608cb
SimonKaresz <kareszkaresz0706@gmail.com>
2022-09-22 16:34:22 +0200

    portfolio v1.0

--- src/app/components/references/references.component.ts
+++ src/app/components/references/references.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferencesComponent implements OnInit {
 
+  porfolio = {
+    ref_p_img: "assets/img/ref_p01.png",
+    alt: "porfolio image",
+    github_pages: "https://simonkaresz.github.io/portfolio/introduction",
+    github_code: "https://github.com/SimonKaresz/portfolio",
+  }
+
+  sample01 = {
+    ref_tda_img: "assets/img/ref_tda01.png",
+    alt: "to do app image",
+    github_pages: "https://simonkaresz.github.io/to_do_app_static/main",
+    github_code: "https://github.com/SimonKaresz/to_do_app_static",
+  }
+
   constructor() { }
 
   ngOnInit(): void {
